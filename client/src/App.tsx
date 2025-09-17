104e0002caf1a3a745bc2396d136d475d2edc7ce
davidkev159 <manchaladavidkevin159@gmail.com>
2025-09-17 15:59:21 +0530

    scaffold: add React App.tsx

---
+++ client/src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div>
+      <h1>X32 AI Mixer</h1>
+      <p>Connect your Behringer X32 and let AI mix your live audio!</p>
+      {/* TODO: Add connection UI, channel meters, AI mix preview */}
+    </div>
+  );
+}
+
+export default App;
